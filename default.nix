fc7f5ecf327042ea3ff67835a0dd4170d0dfe453
iand <dupont@margin.re>
2025-03-26 12:47:32 -0400

    asdf

--- default.nix
+++ default.nix
@@ -172,7 +172,7 @@ in rec {
             ln -s $lockedInput "$BR2_DL_DIR/$(basename $lockedInput)"
         done
 
-        ${makeFHSEnv}/bin/make-with-fhs-env BR2_JLEVEL=$NIX_BUILD_CORES ${envDeclarations} usb_manager
+        ${makeFHSEnv}/bin/make-with-fhs-env BR2_JLEVEL=$NIX_BUILD_CORES ${envDeclarations}
         # ${makeFHSEnv}/bin/make-with-fhs-env BR2_JLEVEL=$NIX_BUILD_CORES ${envDeclarations} sdk
       '';
 
